09d6fa388c008e96e8b0f64173915faf7d5a36de
Julian Mostbauer <julian.mostbauer@outlook.com>
2026-02-10 08:19:42 +0100

    fixed tailwind installation and config files

--- frontend/drivingplaner/src/App.vue
+++ frontend/drivingplaner/src/App.vue
@@ -3,15 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
+  <div class="flex flex-col items-center justify-center gap-4">
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+  </div>
 </template>
 
 <style scoped>
@@ -21,9 +18,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
