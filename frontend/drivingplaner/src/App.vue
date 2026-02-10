3805c09feb7e87d8740189bf5d4a829d4545f5b7
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-02-10 09:21:29 +0100

    first version of navbar

--- frontend/drivingplaner/src/App.vue
+++ frontend/drivingplaner/src/App.vue
@@ -1,29 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
-  <div class="flex flex-col items-center justify-center gap-4">
-    <p>1</p>
-    <p>2</p>
-    <p>3</p>
-  </div>
+  <Navbar />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ frontend/drivingplaner/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="bg-black p-4 border-b border-amber-500">
+
+    <div class="inline-flex space-x-4">
+      <a
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          :class="[
+            item.current ? 'bg-zinc-900 text-amber-500 shadow-inner' : 'text-amber-300/70 hover:text-amber-300 hover:bg-zinc-900/50',
+                            'px-4 py-2 rounded-md text-1xl font-semibold transition-all duration-200'
+            ]"
+      >
+        {{ item.name }}
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+const navigation = [
+  { name: 'Home', href: '#', active: true },
+  { name: 'Calendar', href: '/calendar', active: false },
+]
+</script>
