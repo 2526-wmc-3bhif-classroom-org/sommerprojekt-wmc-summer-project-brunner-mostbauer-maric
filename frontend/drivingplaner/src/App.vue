90dc0421c79f0501e8143548a3495d62a52b7510
Julian Mostbauer <julian.mostbauer@outlook.com>
2026-02-11 08:33:28 +0100

    Added Footer component

--- frontend/drivingplaner/src/App.vue
+++ frontend/drivingplaner/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 </script>
 
 <template>
-  <div class="overflow-x-hidden min-h-screen bg-black">
-  <Navbar />
+  <div class="overflow-x-hidden min-h-screen bg-black text-white">
+    <Navbar/>
+    <Footer/>
   </div>
 </template>
 
---
+++ frontend/drivingplaner/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<footer class="flex flex-col items-center justify-center w-full bg-black text-gray-500 ">
+  © 2026 by Brunner Jan, Maric Luka, Mostbauer Julian
+</footer>
+</template>
+
+<style scoped>
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+</style>
