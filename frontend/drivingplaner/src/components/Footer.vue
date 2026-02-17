ca9edc70a06fa09507a05044892e549a586f7375
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-02-17 17:21:37 +0100

    removed css style in footer and added tailwind instead

--- frontend/drivingplaner/src/components/Footer.vue
+++ frontend/drivingplaner/src/components/Footer.vue
@@ -3,16 +3,11 @@
 </script>
 
 <template>
-<footer class="flex flex-col items-center justify-center w-full bg-black text-gray-500 ">
+<footer class="flex flex-col items-center justify-center w-full bg-black text-gray-500 fixed bottom-0 left-0">
   © 2026 by Brunner Jan, Maric Luka, Mostbauer Julian
 </footer>
 </template>
 
 <style scoped>
-  footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-  }
+
 </style>
