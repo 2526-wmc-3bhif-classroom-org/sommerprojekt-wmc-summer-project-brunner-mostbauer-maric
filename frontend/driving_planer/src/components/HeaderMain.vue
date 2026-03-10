6757a7a33d1e0c6528bfda00187cdf8a3ff0cd2a
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-03-10 09:32:18 +0100

    added header component

---
+++ frontend/driving_planer/src/components/HeaderMain.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const props= defineProps({
+    title: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <div class="header align-items-center p-10">
+    <Motion
+      is="h1"
+      class="text-7xl font-bold text-black text-center"
+      v-motion-pop-visible-once
+      :duration="600"
+    >{{title}}</Motion>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
