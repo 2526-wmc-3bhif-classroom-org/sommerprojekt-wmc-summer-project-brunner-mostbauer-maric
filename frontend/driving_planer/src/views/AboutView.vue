3739c2292512b96b11712fc9f432e0775d31c2c1
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-03-12 07:08:11 +0100

    started to implement the about page

--- frontend/driving_planer/src/views/AboutView.vue
+++ frontend/driving_planer/src/views/AboutView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import Card from 'primevue/card'
-import Divider from 'primevue/divider'
+import HeaderMain from "@/components/HeaderMain.vue";
 </script>
 
 <template>
-
+  <div>
+    <HeaderMain title="Wer sind wir"
+                description="Wir sind ein Team von Fahrlehrern, die es sich zum Ziel gesetzt haben, die Fahrschulausbildung zu revolutionieren. Mit unserer App möchten wir den Lernprozess für Fahrschüler einfacher, effizienter und unterhaltsamer gestalten."
+                :duration="400"
+                :desktopHeight="'text-5xl'"
+                :mobileHeight="'text-3xl'"
+    />
+  </div>
 </template>
 
 
